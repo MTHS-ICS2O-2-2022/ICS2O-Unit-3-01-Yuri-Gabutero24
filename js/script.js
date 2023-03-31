271caeee26f723784a8ab050c928aaccb41640e6
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-03-31 17:06:32 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function is for calculating the area of a trapezoid
+ */
+function calculate() {
+ // input
+  const abase = parseInt(document.getElementById('a-base').value)
+  const bbase = parseInt(document.getElementById('b-base').value)
+  const trapezoidheight = parseInt(document.getElementById('height').value)
+
+  // process
+  const area = ((abase + bbase) / 2) * trapezoidheight
+
+  // output
+  document.getElementById ('area-of-trapezoid').innerHTML = "The Area of your trapezoid is: " + area + " cm² "
+}
